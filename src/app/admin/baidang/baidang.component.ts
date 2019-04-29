827a329631505035cc902cacb181834f0962658e
havanduy <havanduy709@gmail.com>
2019-04-29 22:08:24 +0700

    no message

--- src/app/admin/baidang/baidang.component.ts
+++ src/app/admin/baidang/baidang.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomService } from 'src/@http-service/room.service';
 import { Toast, ToastrService } from 'ngx-toastr';
-
+import { TaoYeuCauThueNhaService } from 'src/@http-service/taoyeucauthuenha.service';
 @Component({
     selector: 'app-baidang',
     templateUrl: './baidang.component.html',
@@ -11,18 +11,20 @@ export class BaidangComponent implements OnInit {
     public rooms: any;
     private selectRoom: any;
     public checksupperadmin: any;
-    constructor(private roomsv: RoomService, private toastr: ToastrService) {
+    public listYeuCauThueNha: any;
+    constructor(private roomsv: RoomService, private toastr: ToastrService, private taoYeuCauThueNhaService: TaoYeuCauThueNhaService) {
         this.checksupperadmin = JSON.parse(localStorage.getItem('data')).user.role;
     }
 
     ngOnInit() {
-        this.get();
+        this.layYeuCauThueNha();
     }
     get() {
         if (this.checksupperadmin == 'admin' || this.checksupperadmin == 'user') {
             this.roomsv.laycacbaidangcuauser()
                 .then((data: any) => {
                     this.rooms = data;
+                    this.laycactrangthai_Baidang();
                 })
                 .catch(err => {
                     return err;
@@ -31,11 +33,30 @@ export class BaidangComponent implements OnInit {
             this.roomsv.getRooms()
                 .then((data: any) => {
                     this.rooms = data.Data;
+                    this.laycactrangthai_Baidang();
                 })
                 .catch(err => {
                     return err;
                 });
         }
+    }
+
+    laycactrangthai_Baidang(){
+        for (let i = 0; i < this.rooms.length; i++) {
+            let trangthai_baidang = 1;
+            for (let j = 0; j < this.listYeuCauThueNha.length; j++) {
+                if(this.rooms[i]._id == this.listYeuCauThueNha[j].idngoinha._id){
+                    if(this.listYeuCauThueNha[j].trangthai == 1){
+                        trangthai_baidang = 2;
+                    }
+                }
+            }
+            this.rooms[i].trangthai_baidang = trangthai_baidang;
+            if(this.rooms[i].trangthai == false){
+                this.rooms[i].trangthai_baidang = 0;
+            }
+        }
+        
     }
     getRoom(room) {
         this.selectRoom = room;
@@ -48,6 +69,16 @@ export class BaidangComponent implements OnInit {
             })
             .catch((err) => {
                 this.toastr.error("error")
+            })
+    }
+
+    layYeuCauThueNha(){
+        this.taoYeuCauThueNhaService.layCacYeuCauThueNhaCuaUser()
+            .then((res) => {
+                this.listYeuCauThueNha = res;
+                this.get();
+            })
+            .catch((err) => {
             })
     }
 
